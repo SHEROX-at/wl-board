5a8eca69c4d9620aa92fd4529e2993ffc2b7a58b
SHEROX-at <tobias.hoerandner@icloud.com>
2026-05-01 00:31:11 +0200

    Update wl.js

--- api/wl.js
+++ api/wl.js
@@ -1,12 +1,25 @@
 export default async function handler(req, res) {
+  const rbl = "602009";
+
   try {
     const response = await fetch(
-      "https://www.wienerlinien.at/ogd_realtime/monitor?rbl=602009"
+      `https://www.wienerlinien.at/ogd_realtime/monitor?rbl=${rbl}`
     );
 
     const data = await response.json();
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    // wenn realtime leer → fallback auf geplante Zeiten
+    if (!data.data.monitors || data.data.monitors.length === 0) {
+
+      const fallback = await fetch(
+        `https://www.wienerlinien.at/ogd_realtime/monitor?rbl=${rbl}&activateTrafficInfo=0`
+      );
+
+      const fallbackData = await fallback.json();
+
+      return res.status(200).json(fallbackData);
+    }
+
     res.status(200).json(data);
 
   } catch (e) {
