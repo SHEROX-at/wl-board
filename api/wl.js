436d56cccc9a98568b4e941fb9c7cecc82dc2d45
SHEROX-at <tobias.hoerandner@icloud.com>
2026-05-01 00:18:11 +0200

    Create wl.js

---
+++ api/wl.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://www.wienerlinien.at/ogd_realtime/monitor?rbl=602009"
+    );
+
+    const data = await response.json();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).json(data);
+
+  } catch (e) {
+    res.status(500).json({ error: "fail" });
+  }
+}
